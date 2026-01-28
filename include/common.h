e9ed24a9fc31efb027ddbc57b62dd9303f52a3c8
Ertaims <762933863@qq.com>
2026-01-28 14:41:29 +0800

    创建模板项目，测试第三方库是否正常工作

---
+++ include/common.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "imgui.h"
+#include "imconfig.h"
+#include "imgui_impl_opengl3.h"
+#include "imgui_impl_sdl3.h"
+#include "imgui_internal.h"
+#include "imstb_rectpack.h"
+#include "imstb_textedit.h"
+#include "imstb_truetype.h"
+
+#include <d3d11.h>
